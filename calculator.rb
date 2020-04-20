f7f48d2b37387dc57c879ff4c76c0569066efff7
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-04-20 17:09:43 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,4 @@ sum = first_number + second_number
 difference = first_number - second_number
 # Add your variables here
 product = first_number * second_number
+quotient = first_number / second_number
